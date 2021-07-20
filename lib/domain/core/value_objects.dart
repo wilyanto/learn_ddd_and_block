1064f29035dbcb2df4d366d40188fa83f8ba4f32
wilyanto <wilyanto24@gmail.com>
2021-07-20 23:11:53 +0700

    adding firebase_auth_facade

--- lib/domain/core/value_objects.dart
+++ lib/domain/core/value_objects.dart
@@ -1,12 +1,16 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 
+import 'errors.dart';
 import 'failures.dart';
 
 @immutable
 abstract class ValueObject<T> {
   const ValueObject();
   Either<ValueFailure<T>, T> get value;
+
+  // id = identity - same as writing (r) => r
+  T getOrCrash() => value.fold((l) => throw UnexpectedValueError(l), id);
 
   bool isValid() => value.isRight();
 
