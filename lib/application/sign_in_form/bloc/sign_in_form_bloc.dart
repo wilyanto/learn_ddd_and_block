b662e69ef8dbdff899addce97d7eddee923b8991
wilyanto <wilyanto24@gmail.com>
2021-07-20 10:05:50 +0700

    adding signInForm BloC in application layer

---
+++ lib/application/sign_in_form/bloc/sign_in_form_bloc.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+
+import 'package:dartz/dartz.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:learn_ddd_and_bloc/domain/auth/auth_failure.dart';
+import 'package:learn_ddd_and_bloc/domain/auth/i_auth_facade.dart';
+import 'package:learn_ddd_and_bloc/domain/auth/value_objects.dart';
+
+part 'sign_in_form_event.dart';
+part 'sign_in_form_state.dart';
+part 'sign_in_form_bloc.freezed.dart';
+
+class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
+  final IAuthFacade _authFacade;
+
+  SignInFormBloc(this._authFacade) : super(SignInFormState.initial());
+
+  @override
+  Stream<SignInFormState> mapEventToState(
+    SignInFormEvent event,
+  ) async* {
+    // TODO: implement mapEventToState
+  }
+}
